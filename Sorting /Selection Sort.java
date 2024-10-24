502ceec0b4cf015466a426d3dfd4b446660b08fb
Kumaran <150506302+kumaran29senthil@users.noreply.github.com>
2024-10-24 23:49:48 +0530

    Selection Sort

---
+++ Sorting /Selection Sort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Solution {
+    static int[] selection_sort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {24, 15, 48, 88, 33};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        selection_sort(arr, n);
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
