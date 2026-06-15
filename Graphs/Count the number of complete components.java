9f88ae6488e32ced8a69612232c6bcf8c1681a74
Kumaran S <150506302+kumaran29senthil@users.noreply.github.com>
2026-06-15 22:28:45 +0530

    Update Count the number of complete components.java

--- Graphs/Count the number of complete components.java
+++ Graphs/Count the number of complete components.java
@@ -39,3 +39,7 @@ class Solution {
         return completeComponentsCount;
     }
 }
+
+// Hint 1 : Find the connected components of an undirected graph using depth-first search (DFS) or breadth-first search (BFS).
+// Hint 2 : For each connected component, count the number of nodes and edges in the component.
+// Hint 3 : A connected component is complete if and only if the number of edges in the component is equal to m*(m-1)/2, where m is the number of nodes in the component.
